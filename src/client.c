83203e8dcae27eb03642633729b666613bb74f38
Daniel Weinman <namniewd@gmail.com>
2019-06-21 09:16:39 -0700

    handling redirects for google

--- src/client.c
+++ src/client.c
@@ -150,6 +150,24 @@ int main(int argc, char *argv[])
     printf("%s\n", buf);
   }
 
+  char *redirect = strstr(buf, "HTTP/1.1 301");
+
+  if (redirect != NULL) {
+    char *loc = strstr(buf, "http");
+    char *endloc = strchr(loc, '\n');
+    *endloc = '\0';
+    
+    urlinfo = parse_url(loc);
+
+    sockfd = get_socket(urlinfo->hostname, urlinfo->port);
+    send_request(sockfd, urlinfo->hostname, urlinfo->port, urlinfo->path);
+
+    while ((numbytes = recv(sockfd, buf, BUFSIZE - 1, 0)) > 0) {
+    // print the data we got back to stdout
+      printf("%s\n", buf);
+    }
+  }
+
   free(urlinfo);
   close(sockfd);
 
